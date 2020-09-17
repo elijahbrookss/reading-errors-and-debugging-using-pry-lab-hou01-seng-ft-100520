79023fd364f95e51e72e2350a501bfc9dce59f7e
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-17 13:22:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,10 @@
 # don't forget to add: require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    string = "ssssssssss" + string
+    10.times do
+      string = "s" + string 
+    end
+    return string
   else
     string
   end
